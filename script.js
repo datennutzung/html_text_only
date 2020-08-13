64d909bfb14e9e059e540ea12381c336f736137f
Miron Gertis <miron@gertis.de>
2020-08-13 18:04:23 +0200

    ⚗ Added HTML tag creation class, changed footer

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+class MyElement {
+    constructor (tag, html, inner) {
+        this.tag = tag;
+        this.inner = inner;
+        this.html = html;
+        this.length = inner.length
+    }
+    
+    toString = function() {
+        return "<"+this.tag+" "+this.html+">"+this.inner+"</"+this.tag+">";
+    }
+}
+
 const state = {
     body: null,
     textWidth: 0,
@@ -45,40 +58,74 @@ function drawPage() {
 }
 
 function createLine(start, end, fill, ...content) {
-    let combLength = String(start).length+String(end).length+content.join('').replace(/&\w+;/gi, '@').length;
-    return ''+start+content.join('')+fill.repeat(state.maxHorizontalChars-combLength)+end;
+    function countContent (content) {
+        let i = 0
+        for (let c of content) {
+            if (typeof c == typeof "string") {
+                i += c.replace(/&\w+;/gi, '@').length;
+            } else {
+                i += c.length;
+            }
+        }
+        return i
+    }
+    let combLength = String(start).length+String(end).length+countContent(content);
+    return ''+start+content.join("")+fill.repeat(state.maxHorizontalChars-combLength)+end;
 }
 
 function getHeader() {
     let headerLines = [];
     headerLines.push(createLine(b.lu, b.ru, b.hs));
     title = window.atob(
-        "IF9fXyAgICBfX18gICBfLHwgICBcICAvICAgfCB8X3wsfCB8XCBcLyAvfCB8ICBfICAgX1\
-        9fICAgX19fX18gICBfX19fXyx8IHwgXF9fLyB8IHwgfCB8IHwgIF98IHwgIF8gIHwgfCAgX\
-        yAgfCx8IHwgICAgICB8IHwgfCB8IHwgfCAgIHwgfF98IHwgfCB8IHwgfCx8X3wgICAgICB8X\
-        3wgfF98IHxffCAgIHxfX19fX3wgfF98IHxffCw=");
-    for (let line of title.split(",")) {
+        `IF9fXyAgICBfX18gICBfLHwgICBcICAvICAgfCB8X3wsfCB8XCBcLyAvfCB8ICBfICAgX1
+        9fICAgX19fX18gICBfX19fXyx8IHwgXF9fLyB8IHwgfCB8IHwgIF98IHwgIF8gIHwgfCAgX
+        yAgfCx8IHwgICAgICB8IHwgfCB8IHwgfCAgIHwgfF98IHwgfCB8IHwgfCx8X3wgICAgICB8X
+        3wgfF98IHxffCAgIHxfX19fX3wgfF98IHxffCw=`);
+    for (let line of title.split(',')) {
         headerLines.push(createLine(b.vs, b.vs, b.sp, " ", line, " "));
     }
     headerLines.push(createLine(b.lt, b.rt, b.hs));
     return headerLines;
 }
 
+function getLongText(text) {
+    let textBlock = [];
+
+}
+
+function getMotD() {
+
+}
+
 function getFooter() {
     let footerLines = []
     footerLines.push(createLine(b.lt, b.rt, b.hs));
-    footerLines.push(createLine(b.vs, b.vs, b.sp, ' &copy; 2020 Miron Gertis'))
-    footerLines.push(createLine(b.ld, b.rd, b.hs))
+    let websiteLink = new MyElement('a', 'href="https://datennutzung.github.io"', 'Website')
+    let githubLink = new MyElement('a', 'href="https://github.com/datennutzung/"', 'GitHub')
+    let twitterLink = new MyElement('a', 'href="https://twitter.com/datennutzung/"', 'Twitter')
+    let emailLink = new MyElement('a', 'href="mailto://miron@gertis.de"', 'Email')
+    let telegramLink = new MyElement('a', 'href="https://t.me/MironGertis"', 'Telegram')
+    footerLines.push(createLine(b.vs, b.vs, b.sp,
+        " ", websiteLink,
+        " ", githubLink,
+        " ", twitterLink,
+        " ", emailLink,
+        " ", telegramLink));
+    footerLines.push(createLine(b.vs, b.vs, b.sp, ' &copy; 2020 Miron Gertis'));
+    footerLines.push(createLine(b.ld, b.rd, b.hs));
     return footerLines;
 }
+
 
 function setBody(windowWidth, windowHeight, textWidth, textHeight) {
     state.maxHorizontalChars = Math.floor(windowWidth / textWidth);
     state.maxVerticalChars = Math.floor(windowHeight / textHeight);
+
     let headerLines = getHeader()
+    let body = []
     let fillerLines = []
     let footerLines = getFooter()
-    let usedLines = headerLines.length + footerLines.length;
+    let usedLines = headerLines.length + body.length + footerLines.length;
     for (let i = 0; i < state.maxVerticalChars-usedLines; i++) {
         fillerLines.push(createLine(b.vs, b.vs, b.sp))
     }
